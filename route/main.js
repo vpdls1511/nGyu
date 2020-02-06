969169b13c6f6b46f3ff3b6e0e20d206b8699f3c
김남규 <kimnamkyu@Kim.local>
2020-02-06 22:32:05 +0900

    test

--- route/main.js
+++ route/main.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const setting = require('./setting');
 
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-
 
 router.get('/', function(req, res, next) {
     res.render('index', {title : setting.title});
