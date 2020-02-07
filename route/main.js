f7e0bdf2a0318aa60ba4a80ad512e4465c1e13cc
김남규 <kimnamkyu@Kim.local>
2020-02-07 15:16:45 +0900

    added logo

--- route/main.js
+++ route/main.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('index', {title : setting.title});
 })
 
-
+router.get('/create' function(req,res,next){
+  res.render('index');
+})
 
 module.exports = router;
